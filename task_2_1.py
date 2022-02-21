77804c6ff252ccd497d152d0a32be592f31422b3
Buki81 <buki81@yandex.ru>
2022-02-21 21:07:53 +0300

    dz_2

---
+++ task_2_1.py
@@ -0,0 +1,11 @@
+# Выяснить тип результата выражений
+
+number_1 = 15 * 3
+number_2 = 15 / 3
+number_3 = 15 // 2
+number_4 = 15 ** 2
+
+print(type(number_1))
+print(type(number_2))
+print(type(number_3))
+print(type(number_4))
